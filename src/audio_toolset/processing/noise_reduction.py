fa3b9b214f075fc261b68df29fa30474a6d042f3
moshes <maxoshe@gmail.com>
2026-05-11 19:16:11 -0700

    add wiener filter noise reduction

--- src/audio_toolset/processing/noise_reduction.py
+++ src/audio_toolset/processing/noise_reduction.py
@@ -5,16 +5,39 @@ from audio_toolset.audio_data import AudioData
 from audio_toolset.util import convert_db_to_factor
 
 
-def apply_spectral_gating(
-    audio_data: AudioData, noise_threshold_db: float = -50, attenuation_db: float = -1
+def _apply_spectral_gating(
+    audio_data: AudioData, noise_threshold: float, attenuation: float
 ) -> AudioData:
     new_data = audio_data.get_copy()
-
-    noise_threshold = convert_db_to_factor(noise_threshold_db)
-    attenuation = convert_db_to_factor(attenuation_db)
-
     signal_fft = fft(new_data.data)
     sample_size = new_data.get_number_of_samples()
     signal_fft[abs(signal_fft) * (2 / sample_size) < noise_threshold] *= attenuation
     new_data.data = np.real(ifft(signal_fft))
     return new_data
+
+
+def _apply_wiener_filter(
+    audio_data: AudioData, noise_threshold: float, attenuation: float
+) -> AudioData:
+    new_data = audio_data.get_copy()
+    signal_fft = fft(new_data.data)
+    sample_size = new_data.get_number_of_samples()
+    normalized_magnitude = np.maximum(abs(signal_fft) * (2 / sample_size), 1e-10)
+    gain = np.maximum(attenuation, 1 - (noise_threshold / normalized_magnitude) ** 2)
+    signal_fft *= gain
+    new_data.data = np.real(ifft(signal_fft))
+    return new_data
+
+
+def apply_noise_reduction(
+    audio_data: AudioData,
+    noise_threshold_db: float = -50,
+    attenuation_db: float = -1,
+    smooth: bool = True,
+) -> AudioData:
+    noise_threshold = convert_db_to_factor(noise_threshold_db)
+    attenuation = convert_db_to_factor(attenuation_db)
+
+    if smooth:
+        return _apply_wiener_filter(audio_data, noise_threshold, attenuation)
+    return _apply_spectral_gating(audio_data, noise_threshold, attenuation)
--- tests/test_noise_reduction.py
+++ tests/test_noise_reduction.py
@@ -4,7 +4,7 @@ from scipy.signal import welch
 from audio_toolset.audio_data import AudioData
 from audio_toolset.constants.frequencies import TEST_TONE_1000HZ
 from audio_toolset.oscillators import generate_white_noise
-from audio_toolset.processing.noise_reduction import apply_spectral_gating
+from audio_toolset.processing.noise_reduction import apply_noise_reduction
 
 
 def test_spectral_gating(test_tone: AudioData):
@@ -15,10 +15,28 @@ def test_spectral_gating(test_tone: AudioData):
         amplitude_dbfs=noise_level_db,
     )
 
-    result = apply_spectral_gating(
-        test_tone,
-        noise_threshold_db=-10,
-        attenuation_db=-10,
+    result = apply_noise_reduction(
+        test_tone, noise_threshold_db=-10, attenuation_db=-10, smooth=False
+    )
+
+    f, psd_pre = welch(test_tone.data, fs=test_tone.sample_rate)
+    _, psd_post = welch(result.data, fs=result.sample_rate)
+
+    mask = f != TEST_TONE_1000HZ
+
+    assert np.all(psd_post[mask] < psd_pre[mask])
+
+
+def test_wiener_filter(test_tone: AudioData):
+    noise_level_db = -20
+    test_tone.data += generate_white_noise(
+        sample_rate_hz=test_tone.sample_rate,
+        duration_s=test_tone.get_duration_s(),
+        amplitude_dbfs=noise_level_db,
+    )
+
+    result = apply_noise_reduction(
+        test_tone, noise_threshold_db=-10, attenuation_db=-10, smooth=True
     )
 
     f, psd_pre = welch(test_tone.data, fs=test_tone.sample_rate)
